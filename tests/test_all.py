52fcfe4265dff1d6791acde2c401ffa63a419798
Bede Constantinides <bedeabc@gmail.com>
2023-11-20 01:05:40 +0000

    Fix test

--- tests/test_all.py
+++ tests/test_all.py
@@ -22,4 +22,7 @@ def test_cli_sars2_tsv():
 
 
 def test_sars2_xxh128():
-    assert lib.sum_nt("tests/data/MN908947.fasta") == {"MN908947.3": "ca5e95436b957f93"}
+    assert lib.sum_nt("tests/data/MN908947.fasta") == (
+        {"MN908947.3": "ca5e95436b957f93"},
+        None,
+    )
